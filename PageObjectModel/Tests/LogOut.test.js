aad945d3122a1484ebb187b6842e97fdd09298dd
VictorGarcia <laurencio.garcia@wizeline.com>
2021-05-26 13:26:22 -0500

    Do It task.

--- PageObjectModel/Tests/LogOut.test.js
+++ PageObjectModel/Tests/LogOut.test.js
@@ -1,20 +1,16 @@
-import LoginPage from '../Pages/LoginPage'
-import {CREDENTIALS} from '../Data/Constant'
-import SideBarPage from '../Pages/SideBarPage'
-import WelcomePage from '../Pages/WelcomePage'
+/* eslint-disable no-undef */
+import LoginPage from '../Pages/LoginPage';
+import { CREDENTIALS } from '../Data/Constant';
+import SideBarPage from '../Pages/SideBarPage';
+import WelcomePage from '../Pages/WelcomePage';
 
 fixture('LogOut feature testing')
-    .page `https://todoist.com/es`
-    .beforeEach(async t=> {
-        await LoginPage.LoginSubmit(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD);   
-        
-    })
+  .page`https://todoist.com/es`
+  .beforeEach(async (t) => {
+    await LoginPage.LoginSubmit(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD);
+  });
 
-    
-    
-    test('LogOut User', async t => {
-        await SideBarPage.LogOut();
-        await t.expect(WelcomePage.BtnIniciarSesion.exists).ok();
-
-    })   
-    
+test('LogOut User', async (t) => {
+  await SideBarPage.LogOut();
+  await t.expect(WelcomePage.BtnIniciarSesion.exists).ok();
+});
--- PageObjectModel/Tests/Login.test.js
+++ PageObjectModel/Tests/Login.test.js
@@ -1,28 +1,25 @@
-import LoginPage from '../Pages/LoginPage'
-import InventoryPage from '../Pages/InventoryPage'
-import {CREDENTIALS} from '../Data/Constant'
-import SideBarPage from '../Pages/SideBarPage'
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+import LoginPage from '../Pages/LoginPage';
+import { CREDENTIALS } from '../Data/Constant';
+import SideBarPage from '../Pages/SideBarPage';
 
 fixture('Login feature testing')
-    .page `https://todoist.com/es`
+  .page`https://todoist.com/es`;
 
+test('Login User Valid', async (t) => {
+  await LoginPage.LoginSubmit(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD);
+  await t.expect(SideBarPage.BtnAvatar.exists).ok();
+});
 
-    test('Login User Valid', async t => {
+test('Login Invalid', async (t) => {
+  await LoginPage.LoginSubmit(CREDENTIALS.INVALID_USER.USERNAME, CREDENTIALS.INVALID_USER.PASSWORD);
+  await t.expect(LoginPage.ErrorMessage.exists).ok();
+  await t.expect(LoginPage.ErrorMessage.innerText).eql('Invalid email address.');
+});
 
-        await LoginPage.LoginSubmit(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD);
-        await t.expect(SideBarPage.BtnAvatar.exists).ok();
-    })   
-    
-    test('Login Invalid', async t => {
-        await LoginPage.LoginSubmit(CREDENTIALS.INVALID_USER.USERNAME, CREDENTIALS.INVALID_USER.PASSWORD);    
-        await t.expect(LoginPage.ErrorMessage.exists).ok();
-        await t.expect(LoginPage.ErrorMessage.innerText).eql('Invalid email address.');
-    })      
-    
-        test('Login Invalid empty fields', async t => {
-        await LoginPage.LoginSubmit("", "");    
-        await t.expect(LoginPage.ErrorMessage.exists).ok();
-        await t.expect(LoginPage.ErrorMessage.innerText).eql('Invalid email address.');
-    })      
-    
-    
+test('Login Invalid empty fields', async (t) => {
+  await LoginPage.LoginSubmit('', '');
+  await t.expect(LoginPage.ErrorMessage.exists).ok();
+  await t.expect(LoginPage.ErrorMessage.innerText).eql('Invalid email address.');
+});
